71acf46a31d9e9aacc41c34e94118b450d77c249
Cyril Scopika <cyril@scopika.com>
2011-11-02 13:42:51 +0100

    Version 1.1 - Le plugin ne prends en copmte que les objets en ligne - Boucle pour les  contenus - Boucle pour les dossiers

--- Sitemaps.class.php
+++ Sitemaps.class.php
@@ -41,8 +41,8 @@ class Sitemaps extends PluginsClassiques{
      * Les sitemaps pour les produits et les rubriques ne peuvent être générées
      * par les boucles normales, trop lentes.
      * On défini donc de nouvelles boucles pour améliorer la génération des sitemaps
+     * @see PluginsClassiques::boucle()
      */
-
     function boucle($texte, $args) {
         $boucle = lireTag($args, "boucle", "string");
         switch($boucle) {
@@ -52,12 +52,23 @@ class Sitemaps extends PluginsClassiques{
             case 'rubrique' :
                 return $this->_boucleRubrique($texte, $args);
                 break;
+            case 'contenu' :
+                return $this->_boucleContenu($texte, $args);
+                break;
+            case 'dossier' :
+                return $this->_boucleDossier($texte, $args);
+                break;
             case 'image' :
                 return $this->_boucleImage($texte, $args);
                 break;
         }
     }
 
+    /**
+     * Boucle pour produits
+     * @param string $texte
+     * @param string $args
+     */
     private function _boucleProduit($texte, $args) {
         $res = '';
 
@@ -68,7 +79,8 @@ class Sitemaps extends PluginsClassiques{
             SELECT
                 produit.id,
                 produit.rubrique
-            FROM ' . Produit::TABLE . ' AS produit', $this->link);
+            FROM ' . Produit::TABLE . ' AS produit
+            WHERE ligne=1', $this->link);
         foreach((array) $resul as $row) {
             if($rewrite->valeur){
                 $resultUrl = CacheBase::getCache()->mysql_query(
@@ -78,50 +90,140 @@ class Sitemaps extends PluginsClassiques{
                         AND param="&id_produit=' . $row->id . '&id_rubrique=' . $row->rubrique . '"'
                         , $this->link);
                         if(!empty($resultUrl)) {
-                            $prodUrl = $urlsite->valeur . '/' . $resultUrl[0]->url;
+                            $url = $urlsite->valeur . '/' . $resultUrl[0]->url;
                         }
             }
-            if(empty($prodUrl)) {
-                $prodUrl = $urlsite->valeur . '/?fond=produit&amp;id_produit=' . $row->id . '&amp;id_rubrique=' . $row->rubrique;
+            if(empty($url)) {
+                $url = $urlsite->valeur . '/?fond=produit&amp;id_produit=' . $row->id . '&amp;id_rubrique=' . $row->rubrique;
             }
-            $temp = str_replace('#URL', $prodUrl, $texte);
+            $temp = str_replace('#URL', $url, $texte);
             $temp = str_replace('#ID', $row->id, $temp);
             $res .= $temp;
         }
         return $res;
     }
 
+     /**
+     * Boucle pour rubriques
+     * @param string $texte
+     * @param string $args
+     */
     private function _boucleRubrique($texte, $args) {
         $res = '';
 
         $urlsite = new Variable("urlsite");
         $rewrite = new Variable("rewrite");
 
-        $resul = CacheBase::getCache()->mysql_query('SELECT id FROM ' . Rubrique::TABLE, $this->link);
+        $resul = CacheBase::getCache()->mysql_query('
+        	SELECT id 
+        	FROM ' . Rubrique::TABLE . ' 
+        	WHERE ligne=1', $this->link);
         foreach((array) $resul as $row) {
             if($rewrite->valeur){
                 $resultUrl = CacheBase::getCache()->mysql_query(
                     'SELECT url from ' . Reecriture::TABLE . '
                     WHERE fond="rubrique" AND param="&id_rubrique=' . $row->id . '"'
+                    , $this->link
+                );
+                if(!empty($resultUrl)) {
+                    $url = $urlsite->valeur . '/' . $resultUrl[0]->url;
+                }
+            }
+            if(empty($url)) {
+                $url = $urlsite->valeur . "/?fond=rubrique&amp;id_rubrique=" . $row->id;
+            }
+            $temp = str_replace('#URL', $url, $texte);
+            $temp = str_replace('#ID', $row->id, $temp);
+            $res .= $temp;
+        }
+        return $res;
+    }
+    
+     /**
+     * Boucle pour contenus
+     * @param string $texte
+     * @param string $args
+     */
+	private function _boucleContenu($texte, $args) {
+        $res = '';
+
+        $urlsite = new Variable("urlsite");
+        $rewrite = new Variable("rewrite");
+
+        $resul = CacheBase::getCache()->mysql_query('
+            SELECT
+                contenu.id,
+                contenu.dossier
+            FROM ' . Contenu::TABLE . ' AS contenu
+            WHERE ligne=1', $this->link);
+        foreach((array) $resul as $row) {
+            if($rewrite->valeur){
+                $resultUrl = CacheBase::getCache()->mysql_query(
+                    'SELECT url from ' . Reecriture::TABLE . '
+                    WHERE
+                        fond="contenu"
+                        AND param="&id_contenu=' . $row->id . '&id_dossier=' . $row->dossier . '"'
+                        , $this->link);
+                        if(!empty($resultUrl)) {
+                            $url = $urlsite->valeur . '/' . $resultUrl[0]->url;
+                        }
+            }
+            if(empty($url)) {
+                $url = $urlsite->valeur . '/?fond=contenu&amp;id_contenu=' . $row->id . '&amp;id_dossier=' . $row->dossier;
+            }
+            $temp = str_replace('#URL', $url, $texte);
+            $temp = str_replace('#ID', $row->id, $temp);
+            $res .= $temp;
+        }
+        return $res;
+    }
+    
+ 	/**
+     * Boucle pour dossier
+     * @param string $texte
+     * @param string $args
+     */
+    private function _boucleDossier($texte, $args) {
+        $res = '';
+
+        $urlsite = new Variable("urlsite");
+        $rewrite = new Variable("rewrite");
+
+        $resul = CacheBase::getCache()->mysql_query('
+        	SELECT id 
+        	FROM ' . Dossier::TABLE . ' 
+        	WHERE ligne=1', $this->link);
+        foreach((array) $resul as $row) {
+            if($rewrite->valeur){
+                $resultUrl = CacheBase::getCache()->mysql_query(
+                    'SELECT url from ' . Reecriture::TABLE . '
+                    WHERE fond="dossier" AND param="&id_dossier=' . $row->id . '"'
                     , $this->link);
                     if(!empty($resultUrl)) {
-                        $rubUrl = $urlsite->valeur . '/' . $resultUrl[0]->url;
+                        $url = $urlsite->valeur . '/' . $resultUrl[0]->url;
                     }
             }
-            if(empty($rubUrl)) {
-                $rubUrl = $urlsite->valeur . "/?fond=rubrique&amp;id_rubrique=" . $row->id;
+            if(empty($url)) {
+                $url = $urlsite->valeur . "/?fond=dossier&amp;id_dossier=" . $row->id;
             }
-            $temp = str_replace('#URL', $rubUrl, $texte);
+            $temp = str_replace('#URL', $url, $texte);
             $temp = str_replace('#ID', $row->id, $temp);
             $res .= $temp;
         }
         return $res;
     }
 
+     /**
+     * Boucle pour images
+     * @param string $texte
+     * @param string $args
+     */
     private function _boucleImage($texte, $args) {
         $res = '';
         $produit = lireTag($args, "produit", "int");
         $rubrique = lireTag($args, "rubrique", "int");
+        $contenu = lireTag($args, "contenu", "int");
+        $dossier = lireTag($args, "dossier", "int");
 
         $objet = null;
         if($produit) {
@@ -131,6 +233,14 @@ class Sitemaps extends PluginsClassiques{
         elseif($rubrique) {
             $objet = 'rubrique';
             $idObjet = $rubrique;
+        }
+    	elseif($contenu) {
+            $objet = 'contenu';
+            $idObjet = $contenu;
+        }
+    	elseif($dossier) {
+            $objet = 'dossier';
+            $idObjet = $dossier;
         }
 
         $urlsite = new Variable("urlsite");
@@ -149,14 +259,9 @@ class Sitemaps extends PluginsClassiques{
         if(!is_null($objet)) {
             $sql .= ' WHERE image.' . $objet . '=' . $idObjet;
         }
-        //var_dump($sql);
-        //exit();
         $resul = CacheBase::getCache()->mysql_query($sql, $this->link);
         foreach((array) $resul as $row) {
             $temp = str_replace('#URL', $urlsite->valeur . '/client/gfx/photos/' . $objet . '/' . $row->fichier, $texte);
-            /*
-             $temp = str_replace('#PRODUIT', $params['produit'], $temp);
-             $temp = str_replace("#COMPT", $compt, $temp);*/
             $res .= $temp;
         }
         return $res;
